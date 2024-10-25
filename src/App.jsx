46f3c4689f4c11aea588068e0e790753b05a5452
Rupali R <rupaliii739@gmail.com>
2024-10-25 10:07:30 +0530

    creating ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Navbar/>
+    <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100">
+    <div className="addTodo">
+      <h2 className='text-lg font-bold'>Add a Todo</h2>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+        <h2 className='text-lg font-bold'>Your Todos</h2>
+      <div className="todos">
+        <div className="todo">
 
+        </div>
+      </div>
+    </div>
     </>
   )
 }
